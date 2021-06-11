e49803b64ac999c7f65f1936787788a814c91989
RATIU5 <jasonmemm@gmail.com>
2021-06-10 20:37:39 -0600

    Added custom hover opacity effect to the nav links using arguments in event handlers

--- script.js
+++ script.js
@@ -3,6 +3,12 @@
 const btnScrollTo = document.querySelector(".btn--scroll-to");
 const section1 = document.querySelector("#section--1");
 
+const nav = document.querySelector(".nav");
+
+const tabs = document.querySelectorAll(".operations__tab");
+const tabsContainer = document.querySelector(".operations__tab-container");
+const tabsContent = document.querySelectorAll(".operations__content");
+
 ///////////////////////////////////////
 // Modal window
 
@@ -82,10 +88,6 @@ document.querySelector(".nav__links")?.addEventListener("click", function (e) {
 /////////////////////////////////////////////////////////////////
 
 // Tabbed component
-const tabs = document.querySelectorAll(".operations__tab");
-const tabsContainer = document.querySelector(".operations__tab-container");
-const tabsContent = document.querySelectorAll(".operations__content");
-
 // Using delegation again
 tabsContainer.addEventListener("click", (e) => {
 	const clicked = e.target.closest(".operations__tab");
@@ -103,6 +105,23 @@ tabsContainer.addEventListener("click", (e) => {
 		.querySelector(`.operations__content--${clicked.dataset.tab}`)
 		?.classList.add("operations__content--active");
 });
+
+function handleNavHover(e) {
+	if (e.target.classList.contains("nav__link")) {
+		const link = e.target;
+		const siblings = link.closest(".nav").querySelectorAll(".nav__link");
+		const logo = link.closest(".nav").querySelector("img");
+
+		siblings.forEach((el) => {
+			if (el !== link) el.style.opacity = this;
+		});
+		logo.style.opacity = this;
+	}
+}
+
+// Menu fade animation
+nav?.addEventListener("mouseover", handleNavHover.bind(0.5));
+nav?.addEventListener("mouseout", handleNavHover.bind(1));
 
 /////////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////////
